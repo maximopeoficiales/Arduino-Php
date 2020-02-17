3c389b6b0d994b1419cd18d947a3b650719d988d
Maximo Apaza Chirhuana <53198604+maximopeoficiales@users.noreply.github.com>
2020-02-17 03:26:52 -0500

    Pass script

--- PhpMailer/email.php
+++ PhpMailer/email.php
@@ -27,7 +27,7 @@ function enviarEmail(){
             $mail->Host = 'smtp.gmail.com';                   // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
             $mail->Username = 'maximopeoficiales@gmail.com';                 // SMTP username
-            $mail->Password = 'comiendopalomas1';                           // SMTP password
+            $mail->Password = '********';                           // SMTP password
             $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                                    // TCP port to connect to
 
